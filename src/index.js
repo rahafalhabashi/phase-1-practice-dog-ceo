0d702222386665f118e334f840553944c3cfca4e
Rahaf Alhabashi <rahafalhabashi@gmail.com>
2022-03-23 10:53:21 -0400

    commit tree 1bf33b760c78cd6721a49d018a645b1d641ffdb3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log('%c HI', 'color: firebrick')
+fetch(`https://dog.ceo/api/breeds/image/random/4${character.message}`)
+.then(resp => resp.json())
+.then(character => character.forEach(console.log(renderImages)))
+
+function renderImages(char) {
+    dogImg = document.createElement('img')
+    dogContainer = document.getElementById('dog-image-container')
+    dogImg.src = char.message
+    dogContainer.append(dogImg)
+}
+
